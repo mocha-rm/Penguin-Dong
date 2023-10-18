ea94a4d19dae2f3104ca4b45b146cc337676f1e8
Park JaeHong <blogerhong@naver.com>
2023-10-18 18:08:20 +0900

    AudioService Update
    AudioService Script Update (초안 작성)

--- Assets/02.Scripts/Common/AudioService/AudioService.cs
+++ Assets/02.Scripts/Common/AudioService/AudioService.cs
@@ -5,18 +5,96 @@ using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 using UniRx;
+using MoreMountains.Feedbacks;
+
 
 public class AudioService : IInitializable, IDisposable
 {
+    public enum SoundType
+    {
+        BGM,
+        SFX,
+        End //for counting
+    }
+
     [Inject] IObjectResolver _container;
 
-    public void Dispose()
-    {
-        
-    }
+    GameObject _soundObject;
+
+    AudioSource[] _audioSources = new AudioSource[(int)SoundType.End];
+    float[] _volumes = new float[(int)SoundType.End];
+
+    IDisposable _disposable;
+
 
     public void Initialize()
     {
-        
+        if (_soundObject == null)
+        {
+            _soundObject = new GameObject() { name = "AudioService" };
+            UnityEngine.Object.DontDestroyOnLoad(_soundObject);
+
+            string[] soundNames = System.Enum.GetNames(typeof(SoundType));
+            for(int i = 0; i < soundNames.Length - 1; i++)
+            {
+                GameObject obj = new GameObject { name = soundNames[i] };
+                _audioSources[i] = obj.AddComponent<AudioSource>();
+
+                _audioSources[i].volume = _volumes[i] = 1.0f;
+                obj.transform.parent = _soundObject.transform;
+            }
+        }
+        else
+        {
+            Debug.LogError($"Audio Service Make Twice!! Please Check");
+        }
+    }
+
+    
+    public void SetVolume(SoundType type, float volume)
+    {
+        if(type >= SoundType.End)
+        {
+            Debug.LogWarning($"{type} is Over MaxCount");
+            return;
+        }
+
+        _volumes[(int)type] = volume;
+        _audioSources[(int)type].volume = volume;
+    }
+
+    public void Stop(SoundType type)
+    {
+        if(type < SoundType.End)
+        {
+            _audioSources[(int)type].Stop();
+        }
+        else
+        {
+            Debug.LogError($"{type} is Over MaxCount");
+        }
+    }
+
+
+    public void Play()
+    {
+
+    }
+
+    public void Dispose()
+    {
+        foreach(AudioSource audioSource in _audioSources)
+        {
+            if(audioSource != null)
+            {
+                audioSource.Stop();
+                audioSource.clip = null;
+            }
+        }
+
+        _audioSources = null;
+
+        _disposable?.Dispose();
+        _disposable = null;
     }
 }
